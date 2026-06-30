f82663437c074acccdd7f1d1fba9c330070d4d28
Po <8546374-po-man@users.noreply.gitlab.com>
2026-06-30 23:19:44 +0800

    fix: update site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://rabbit.plantbased.studio',
+  site: 'https://lantaurabbit.com',
   i18n: {
     defaultLocale: 'zh-hk',
     locales: ['zh-hk', 'en'],
