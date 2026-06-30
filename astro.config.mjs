66d72f07651b8c4dd07115d7afafc1e9546ae72b
Po <8546374-po-man@users.noreply.gitlab.com>
2026-06-30 23:50:21 +0800

    fix: update site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://lantaurabbit.com',
+  site: 'https://www.lantaurabbit.com',
   i18n: {
     defaultLocale: 'zh-hk',
     locales: ['zh-hk', 'en'],
