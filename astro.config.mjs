055e0f920930ae3eb9aaf9bd07e3bd393d79aae9
Po <8546374-po-man@users.noreply.gitlab.com>
2026-06-30 18:08:14 +0800

    build: add deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://po-man.github.io',
+  base: '/rabbitry',
   i18n: {
     defaultLocale: 'zh-hk',
     locales: ['zh-hk', 'en'],
